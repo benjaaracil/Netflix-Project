ec51d6806fe9bae07d0713013762ac8bd27e041c
benjaaracil <benjaminaracil@gmail.com>
2021-12-04 01:28:36 -0300

    3 differents movie lists

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,59 @@
 import "./Home.scss";
-import React from 'react';
+import {React, useRef, useState, useEffect} from 'react';
 import { AcUnit } from "@material-ui/icons";
 import NavBar from "../components/NavBar/NavBar"
 import Featured from "../components/Featured/Featured";
 import List from "../components/List/List";
+import axios from "axios";
 
 export default function Home() {
+    const [movies, setMovies] = useState();
+    const [movies1, setMovies1] = useState();
+    const [movies2, setMovies2] = useState();
+    let url1 =`https://api.themoviedb.org/3/discover/movie?api_key=6d576e164fd97d2d38823c27321b1b21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`;
+    let url2 = `https://api.themoviedb.org/3/discover/movie?api_key=6d576e164fd97d2d38823c27321b1b21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=2&with_watch_monetization_types=flatrate`;
+    let url3 = `https://api.themoviedb.org/3/discover/movie?api_key=6d576e164fd97d2d38823c27321b1b21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=3&with_watch_monetization_types=flatrate`;
+    const requestOne = axios.get(url1);
+    const requestTwo = axios.get(url2);
+    const requestThree = axios.get(url3);
+
+
+    useEffect(() => {
+        getMovies()
+    }, [])
+
+    const getMovies = () => {
+        axios.all([requestOne, requestTwo, requestThree]).then(axios.spread((...responses) => {
+            const responseOne = responses[0]
+            const responseTwo = responses[1]
+            const responesThree = responses[2]
+            // use/access the results
+            setMovies(responseOne.data.results)
+            setMovies1(responseTwo.data.results)
+            setMovies2(responesThree.data.results)
+            console.log("Primera", responseOne.data.results)
+            console.log("Segunda", responseTwo.data.results) 
+          })).catch(errors => {
+            // react on errors.
+            console.error(errors)
+          })
+
+
+        // .then((response) => {
+        //     setMovies(response.data.results)
+        //     console.log("Dentro del getMovies", movies)
+        // })
+        // .catch((err) => console.error(err));
+    };
+
+
     return (
         <div className="home">
             <NavBar/>
             <Featured type="movie"/>
-            <List name={"Popular on Netflix"}/>
-            <List name={"Continue Watching for Benja"}/>
-            <List name={"Trending now"}/>
+            <List movies = {movies} name={"Popular on Netflix"}/>
+            <List movies = {movies1} name={"Continue Watching for Benja"}/>
+            <List movies = {movies2} name={"Trending now"}/>
         </div>
     )
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,11 +4,11 @@ import ListItem from "../ListItem/ListItem";
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
 import axios from "axios";
 
-export default function List({name}) {
+export default function List({name, movies}) {
     const [slideNumber, setSlideNumber] = useState(0) //Esto nos va a servir para que no se pueda seguir moviendo el "carrusel" si no hay mas elementos hacia la izquierda o derecha
     const [isMove, setIsMoved] = useState(false); //Para desvanecer el botón de ir hacia atras si estamos en el primer elemento...
     const listRef = useRef(); //En vez de usar elementById o algo similar uso el hook useRef de react para hacer referencia a ese componente
-    const [movies, setMovies] = useState();
+    // const [movies, setMovies] = useState();
     let portada = "https://image.tmdb.org/t/p/w500"
     let indice = 0;
 
@@ -27,18 +27,18 @@ export default function List({name}) {
         }
     }
 
-    useEffect(() => {
-        getMovies()
-    }, [])
+    // useEffect(() => {
+    //     getMovies()
+    // }, [])
 
-    const getMovies = () => {
-        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=6d576e164fd97d2d38823c27321b1b21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`)
-        .then((response) => {
-            setMovies(response.data.results)
-            console.log("Dentro del getMovies", movies)
-        })
-        .catch((err) => console.error(err));
-    };
+    // const getMovies = () => {
+    //     axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=6d576e164fd97d2d38823c27321b1b21&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_watch_monetization_types=flatrate`)
+    //     .then((response) => {
+    //         setMovies(response.data.results)
+    //         console.log("Dentro del getMovies", movies)
+    //     })
+    //     .catch((err) => console.error(err));
+    // };
 
 if (movies != undefined){
     return (
